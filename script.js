2af43c7ebd2bbc43f917cf4e3c91ffa52253d5aa
CarterBryantt <68485516+CarterBryantt@users.noreply.github.com>
2022-03-09 13:40:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -245,7 +245,7 @@ async function share() {
 		await navigator.share({
 			title: 'Wordle',
 			text: `I was able to solve Wordle #${wordleIndex} in ${guessCount}/6 tries!\nHow about you?\n${emojis}`,
-			url: 'https://codepen.io/CarterBryant/full/GROPrEN'});
+			url: 'https://carterbryantt.github.io/Sharedle/'});
 	} catch(err) {
 		console.log(err);
 	}
